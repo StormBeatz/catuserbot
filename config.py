a460b5acc4054c754ffb98ec68d49460f06bc8bd
Sтσям <118429184+StormBeatz@users.noreply.github.com>
2023-09-21 14:51:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://jfgubssg:5cLUNg8lS-WdM3CIMI4ca6bO9819zBLs@berry.db.elephantsql.com/jfgubssg"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "BQFVwaUAOUjv3EhDAPBFfoYereCkg3XQqZDIxPUrNIpqRvHsctpuwlqZCh-iUHMICxnKvkgvkd-K0FJO8CbFFwVzzFLgsh2fzkuYTpg8dc1Bi4JuTKNEs46hljfG82MEuBJXknYf-BHHVybsNtaz6vnrp0f_l35AwRCDW1sJcqEAQfkvVKL_zUF2qyuuIFALjoE2cJnR-h6YwqHV5g4vs_L8x9htkuQIJOwNIMuI6eysoZkF1CU5K2J7VVKkhrpG01Geg0GvxJPpV6Bl_DZuYh2licVBRDr_080sBYpBOlvADVgpa37s1heYRUIAd9g1sBxKIVeehm4yE_nBbYSbyri6OfOxKQAAAAGB-BwCAA"
+    STRING_SESSION = "1BVtsOLgBux95RaY4xJCeY-t3JlSZwpuI5YNyEe-_ulmUm_va_n5ak3EqbeS-skvweb5bfke_Jvh1Dpwt6Yd117ti8tVxa5kAKy_5Z7v3NfhDCB7ZF8e592KMxwZlt07iglflFVz18EaZ1Ft3OTxM57rhP0COV9GlHvFueKrjaM7-kOdt7iHBNAqAzCP1X4jY7lTGjh4Et2E03Xkecm_Zv07xELerMYBuXOU4F1lzWx2szYLBQAtb0Ff1nCfp-kIGIAzKps-YiB3DEPy0cMAHHdp-6-CuBzTecw2cQ5YKQV8csjpQxwySIhd0prrZ-VW7Yq2_bxB4YlRMu8PTGGp8NtheW6rhTqI="
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "6660105397:AAG0FGSX6GUaMiOm4a0OSC10fKIhR_0AnJY"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
